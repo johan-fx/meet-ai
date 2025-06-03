bd60fd0527864ad0306deb0fe528a4d970408d29
JohanFX <sosa.johan@gmail.com>
2025-06-03 18:08:05 +0200

    feat(dialog): Introduce responsive dialog component and enhance command functionality
    * feat(dialog): Introduce responsive dialog component and enhance command functionality
    
    - Added a new ResponsiveDialog component that adapts between a dialog and a drawer based on mobile device detection.
    - Updated CommandDialog to support responsive behavior, allowing for better user experience on mobile devices.
    - Enhanced the global CSS to apply a pointer cursor to buttons, improving UI interactivity.
    
    These changes improve the application's responsiveness and usability, particularly for mobile users, by providing a consistent dialog experience across different screen sizes.
    
    * fix(dialog): Update onOpenChange prop type and conditionally render description
    
    - Changed the onOpenChange prop type from a void function to accept a boolean parameter, allowing for better state management.
    - Updated the rendering of the DrawerDescription to only display when a description is provided, improving the component's usability and preventing unnecessary rendering.
    
    These changes enhance the functionality and clarity of the ResponsiveDialog component, ensuring it behaves as expected in various scenarios.
    
    ------
    
    Ref: #9

---
+++ src/components/responsive-dialog.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useIsMobile } from "@/hooks/use-mobile";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from "./ui/drawer";
+
+interface ResponsiveDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  title: string;
+  description?: string;
+  children: React.ReactNode;
+}
+
+const ResponsiveDialog = ({
+  open,
+  onOpenChange,
+  title,
+  children,
+  description,
+}: ResponsiveDialogProps) => {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{title}</DrawerTitle>
+            {!!description && (
+              <DrawerDescription>{description}</DrawerDescription>
+            )}
+          </DrawerHeader>
+          <div className="p-4">{children}</div>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ResponsiveDialog;
