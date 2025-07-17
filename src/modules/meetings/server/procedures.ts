70597a16be9e9edbcbb7547b6552a2ee0da2a530
Johan Sosa <sosa.johan@gmail.com>
2025-07-17 19:51:13 +0200

    fix(meetings): Remove unused token expiration parameters in user token generation
    - Eliminated the `expirationTime` and `issuedAt` parameters from the user token generation process in the meetings router.
    - This change simplifies the token creation logic by focusing on the essential user ID, improving code clarity and maintainability.
    
    These modifications streamline the token generation process, ensuring that only necessary parameters are utilized, which enhances overall code readability.

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -287,12 +287,8 @@ export const meetingsRouter = createTRPCRouter({
 			},
 		]);
 
-		const expirationTime = Math.floor(Date.now() / 1000) + 60 * 60; // 1 hour
-		const issuedAt = Math.floor(Date.now() / 1000) - 60;
 		const token = streamVideo.generateUserToken({
 			user_id: ctx.auth.user.id,
-			exp: expirationTime,
-			validity_in_seconds: issuedAt,
 		});
 
 		return token;
